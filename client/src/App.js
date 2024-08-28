c4f3e5d761babeba1d3b2d856411660f3b4bd6b2
purvisanghani02 <purvisanghani02@gmail.com>
2024-08-28 13:30:16 +0530

    change localhost to deploy backend link

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,9 @@ function App() {
   const [message, setmessage] = useState("");
   useEffect(() => {
     const getdata = async () => {
-      const res = await axios.get("http://localhost:4000/get-data");
+      const res = await axios.get(
+        "https://render-demo-backend-jlp8.onrender.com/get-data"
+      );
       if (res.status === 200) {
         setmessage(res.data.res);
       }
