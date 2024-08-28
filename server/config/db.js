d51e6203808e86652a5cf3c5cf11f887a4296290
purvisanghani02 <purvisanghani02@gmail.com>
2024-08-28 13:22:22 +0530

    db

--- server/config/db.js
+++ server/config/db.js
@@ -1,11 +1,10 @@
 const mongoose = require("mongoose");
 
 const mongoDBUrl = process.env.MONGO_URL;
-const databaseName = process.env.DATABASE;
 
 const connectdb = async () => {
   try {
-    const urlWithDatabase = `${mongoDBUrl}${databaseName}`;
+    const urlWithDatabase = `${mongoDBUrl}`;
     await mongoose.connect(urlWithDatabase);
     console.log("db connect successfully");
   } catch (error) {
